2c1cc6d4cf984d3589a36acf66a4f416c407198d
Mateus Abdallah <abdallahmateus@gmail.com>
2022-11-02 12:56:44 -0700

    inicial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+function addText(){    
+    const welcome = document.getElementById('welcome');
+    welcome.outerHTML =  "<h2>Hi there, my name is Mateus Abdallah I'm a Front End Developer and click in the button below to go to my GitHub and see my projects</h2>"; 
+}
+
