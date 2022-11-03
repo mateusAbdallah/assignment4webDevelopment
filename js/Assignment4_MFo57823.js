f85c600619e244f7332468703b51fbd8a01f54ae
Mateus Abdallah <abdallahmateus@gmail.com>
2022-11-03 15:23:25 -0700

    set functions to validate datas

--- js/Assignment4_MFo57823.js
+++ js/Assignment4_MFo57823.js
@@ -1,5 +1,40 @@
-function addText(){    
-    const welcome = document.getElementById('welcome');
-    welcome.outerHTML =  "<h2>Hi there, my name is Mateus Abdallah I'm a Front End Developer and click in the button below to go to my GitHub and see my projects</h2>"; 
+function runValidate (form){
+    validateName(form);
+    validateId (form);
+    return false;
 }
+
+function validateName (form){
+    var name = form.elements["fullName"];
+
+    if(name.validity.valueMissing){
+        name.setCustomValidity("Please enter your name here");
+        return false;
+    }
+    else {
+        name.setCustomValidity("");
+        return true;
+    }
+}
+
+function validateId (form){
+    var memberId = form.elements["memberId"];
+
+    if(memberId.validity.valueMissing){
+        memberId.setCustomValidity("Please enter your member ID here");
+        return false;
+    }
+    else if(memberId.validity.patternMismatch){
+        memberId.setCustomValidity("Enter a valid ID, start with M and 5 digit code");
+        return false;
+    }
+    else{
+        memberId.setCustomValidity("");
+        return true;
+    }
+}
+
+// function getName() {
+//     var name = document.getElementById("fullName");
+// }
 
